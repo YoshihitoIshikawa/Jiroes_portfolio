1d22b60760b83ee4304b0fa28d39026df0ef267d
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-08-18 22:38:06 +0900

    postsコントローラーを作成

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

cbc62e29d8c583cd257a42655b4c1f41f189eb08
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-08-20 15:52:41 +0900

    fix:postsをshopsコントローラーに書き換え
    shops関連を作るべきところを間違えてpostsとしてしまっていたのでコントローラーの記述修正、以前のpostモデル作成というコミットも正しくはshopモデル

---
+++ back/app/controllers/api/v1/shops_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::ShopsController < ApplicationController
+  def index
+    @shops = Shop.all
+    render json: @shops
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      render json: @shop, status: :created
+    else
+      render json: @shop.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @shop = Shop.find(params[:id])
+    render json: @shop
+  end
+
+  def update
+    @shop = Shop.find(params[:id])
+    if @shop.update(shop_params)
+      render json: @shop
+    else
+      render json: @shop.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @shop.destroy
+  end
+
+  private
+  def shop_params
+    params.require(:shop).permit(:name,:post_id, :address, :phone_number, :access, :parking, :open_time, :closed_days, :remarks, :prohibited_matters, when_to_buy_tickets, call_timing)
+end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/create'
-  get 'posts/show'
-  get 'posts/update'
-  get 'posts/delete'
+  namespace :api do
+    namespace :v1 do
+      resources :shops, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

c6942330160b4590950fda108b7e8798a3d7a126
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-08-25 15:28:31 +0900

    :add:店舗登録機能実装
    react hook formとuseStateの両方で入力値を管理していた為に起きていたと推測される、onSubmitイベントが発火しない問題を解決し実装完了

--- front/pages/shops/new.js
+++ front/pages/shops/new.js
@@ -1,54 +1,21 @@
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { Box, Button, TextField } from '@mui/material';
 import axios from "axios";
-import { useState } from "react";
 import { useRouter } from "next/router";
 import * as yup from "yup";
 import { yupResolver } from '@hookform/resolvers/yup';
 
-const NewShop = () => {
+export default function NewShop() {
   const schema = yup.object({
     name: yup.string().required('店舗名は入力必須項目です。')
   });
 
-  const { control, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
+  const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
   const router = useRouter();
 
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [phone_number, setPhone_number] = useState("");
-  const [access, setAccess] = useState("");
-  const [parking, setParking] = useState("");
-  const [open_time, setOpen_time] = useState("");
-  const [closed_days, setClosed_days] = useState("");
-  const [remarks, setRemarks] = useState("");
-  const [prohibited_matters, setProhibited_matters] = useState("");
-  const [when_to_buy_tickets, setWhen_to_buy_tickets] = useState("");
-  const [call_timing, setCall_timing] = useState("");
-
-  const onChange = (e, setState) => {
-    setState(e.target.value);
-    console.log(name)
-  };
-
-  const onSubmit = async () => {
-    const params = {
-    name: name,
-    address: address,
-    phone_number: phone_number,
-    access: access,
-    parking: parking,
-    open_time: open_time,
-    closed_days: closed_days,
-    remarks: remarks,
-    prohibited_matters: prohibited_matters,
-    when_to_buy_tickets: when_to_buy_tickets,
-    call_timing: call_timing
-    };
-
-    console.log(name)
+  async function onSubmit(data) {
     try {
-      await axios.post("http://localhost:3000/api/v1/shops", params)
+      await axios.post("http://localhost:3000/api/v1/shops", data)
       router.push("/")
     } catch (err) {
       alert("登録に失敗しました。")
@@ -61,203 +28,115 @@ const NewShop = () => {
         <h1 className="text-4xl mb-8">新規店舗登録</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Box mb={2}>
-            <Controller
-              name="name"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="店舗名(必須)"
-                  variant="outlined"
-                  fullWidth
-                  error={ errors.name ? true : false }
-                  helperText={ errors.name?.message }
-                  value={name}
-                  onChange={(e) => {onChange(e, setName)}}
-                />
-              )}
+            <TextField
+              { ...register('name') }
+              label="店舗名(必須)"
+              variant="outlined"
+              fullWidth
+              error={ errors.name ? true : false }
+              helperText={ errors.name?.message }
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="address"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="住所"
-                  variant="outlined"
-                  fullWidth
-                  value={address}
-                  onChange={(e) => {onChange(e, setAddress)}}                />
-              )}
+            <TextField
+              { ...register('address') }
+              label="住所"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={2}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="access"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="アクセス"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={2}
-                  value={access}
-                  onChange={(e) => {onChange(e, setAccess)}}                />
-              )}
+            <TextField
+              { ...register('access') }
+              label="アクセス"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={2}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="open_time"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="営業時間"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={2}
-                  value={open_time}
-                  onChange={(e) => {onChange(e, setOpen_time)}}                />
-              )}
+            <TextField
+              { ...register('open_time') }
+              label="営業時間"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={2}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="closed_days"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="定休日"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={2}
-                  value={closed_days}
-                  onChange={(e) => {onChange(e, setClosed_days)}}                />
-              )}
+            <TextField
+              { ...register('closed_days') }
+              label="定休日"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={2}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="phone_number"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="電話番号"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={2}
-                  value={phone_number}
-                  onChange={(e) => {onChange(e, setPhone_number)}}                />
-              )}
+            <TextField
+              { ...register('phone_number') }
+              label="電話番号"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={2}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="parking"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="駐車場(有・無・その他詳細)"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={2}
-                  value={parking}
-                  onChange={(e) => {onChange(e, setParking)}}                />
-              )}
+            <TextField
+              { ...register('parking') }
+              label="駐車場(有・無・その他詳細)"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={2}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="when_to_buy_tickets"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="食券購入タイミング(列に並ぶ前・入店時等)"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={3}
-                  value={when_to_buy_tickets}
-                  onChange={(e) => {onChange(e, setWhen_to_buy_tickets)}}                />
-              )}
+            <TextField
+              { ...register('when_to_buy_tickets') }
+              label="食券購入タイミング(列に並ぶ前・入店時等)"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={4}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="call_timing"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="コールタイミング(食券提出時・商品提供直前等)"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={3}
-                  value={call_timing}
-                  onChange={(e) => {onChange(e, setCall_timing)}}                />
-              )}
+            <TextField
+              { ...register('call_timing') }
+              label="コールタイミング(食券提出時・商品提供直前等)"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={4}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="prohibited_matters"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="禁止事項(マシマシ不可・麺固め不可・特定のルール等)"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={4}value={prohibited_matters}
-                  onChange={(e) => {onChange(e, setProhibited_matters)}}
-                />
-              )}
+            <TextField
+              { ...register('prohibited_matters') }
+              label="禁止事項(マシマシ不可・麺固め不可・特定のルール等)"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={4}
             />
           </Box>
           <Box mb={2}>
-            <Controller
-              name="remarks"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  { ...field }
-                  label="備考(店舗特有のルール・留意ポイント等)"
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={4}
-                  value={remarks}
-                  onChange={(e) => {onChange(e, setRemarks)}}                />
-              )}
+            <TextField
+              { ...register('remarks') }
+              label="備考(店舗特有のルール・留意ポイント等)"
+              variant="outlined"
+              fullWidth
+              multiline
+              rows={4}
             />
           </Box>
-          <p className="mb-5 text-red-600">{ errors.name?.message }</p>
           <Button sx={{width: 100, marginBottom: 10}} variant="outlined" type="submit">
             送信
           </Button>
@@ -266,5 +145,3 @@ const NewShop = () => {
     </div>
   )
 };
-
-export default NewShop;

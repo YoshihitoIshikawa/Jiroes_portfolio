4965ee68c0a32b912dd87dfcec745661cf25b6b5
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-09-04 18:06:52 +0900

    :pending:ユーザー情報変更機能中断
    試行錯誤したがうまく実装出来なかったので一度中断し他の実装を先行する

--- front/next.config.js
+++ front/next.config.js
@@ -9,6 +9,11 @@ module.exports = {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
       config.resolve.fallback = {
         fs: false,
+        tls: false,
+        net: false,
+        dns: false,
+        module: false,
+        async_hooks: false,
       };
     }
 

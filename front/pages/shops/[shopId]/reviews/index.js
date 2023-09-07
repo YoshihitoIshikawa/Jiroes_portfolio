d5776e42ae1ea629f31c8c33890217b43db56d42
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-09-07 15:38:02 +0900

    :fix:userがローカルデータベースに登録されない
    auth0のデータベースでユーザー管理しているため自前のデータベースにユーザー登録されない問題をauth0のpost user registration hookを使用して解決する為にまずデータベースを公開する手段としてngrokの設定を実装

--- front/pages/shops/[shopId]/reviews/index.js
+++ front/pages/shops/[shopId]/reviews/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 import { useRouter } from 'next/router';
 
 const index = () => {
+  const { user, isLoading, isAuthenticated } = useAuth0();
   const router = useRouter();
   const { shopId } = router.query;
-  console.log({shopId});
+  console.log({ user });
 
-  return <h1>Hello World/{shopId}</h1>;
+  if (isLoading) {
+    return (
+      <div className="flex justify-center mt-20">
+        <div className="sm:w-1/2 flex flex-col">
+          <h2 className="text-4xl">Loading...</h2>
+        </div>
+      </div>
+    )
+  }
+
+  if (isAuthenticated) {
+    return(
+      <h1>Hello World/{shopId}{ user.nickname }</h1>
+    )
+  } else {
+    return(
+      <h1>ログインして下さい。</h1>
+    )
+  }
 };
 
 export default index;

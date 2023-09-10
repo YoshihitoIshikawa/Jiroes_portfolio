6f60e3ec810605ae48a4bb3463be575e7cd3d5da
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-09-10 13:22:58 +0900

    :add:画像加工処理の為にminimagickをinstall
    image_uploader.rbにminimagick関連のコード追記

--- back/app/uploaders/image_uploader.rb
+++ back/app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -27,16 +27,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process scale: [200, 200]
   #
-  # def scale(width, height)
-  #   # do something
-  # end
+  def scale(width, height)
+    resize_to_fit:
+  end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:

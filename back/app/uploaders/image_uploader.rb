1ddba3c3a6e99c54c35d223713967260c2bc855e
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-09-20 16:44:41 +0900

    :update:アップロード画像のサイズ調整
    解像度が低すぎる為上方修正

--- back/app/uploaders/image_uploader.rb
+++ back/app/uploaders/image_uploader.rb
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [200, 200]
+  process resize_to_fit: [1000, 1000]
   #
   # def scale(width, height)
   #   do something
@@ -35,7 +35,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fit: [400, 400]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
